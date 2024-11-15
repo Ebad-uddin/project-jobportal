4f2d3121f6fa70f359eabb08ce7d04b9769a00a8
ZOBIAKAMAL <zoiakamal2003@gmail.com>
2024-11-15 10:54:01 +0500

    done till category

---
+++ add_category.php
@@ -0,0 +1,34 @@
+<?php
+include('config.php');
+
+
+
+
+if(isset($_POST['submit'])){
+
+    $name = $_POST['name'];
+    $description = $_POST['description'];
+    $file_name = $_FILES['icon']['name'];
+    $tmp_name = $_FILES['icon']['tmp_name'];
+
+    move_uploaded_file($tmp_name,'uploads/' . $file_name);
+
+    $query = "INSERT INTO `category` ( `name`, `icon`, `description`) VALUES ( '$name', '$description', '$file_name')";
+
+    $connection_query = mysqli_query($connection, $query);
+
+    if($connection_query){
+        echo "<script>
+        alert('Category Added Successfully');
+        <script>
+        window.location.href = 'category.php';
+        ";
+
+    }
+
+}
+
+    ?>
+
+
+?>
